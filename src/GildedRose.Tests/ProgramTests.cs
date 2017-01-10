a7bac37aa4626b60571fa5da13920ef870d90f15
Phil Wright <pwright@foxguardsolutions.com>
2017-01-10 12:13:28 -0500

    Set up project rules and Nunit

---
+++ src/GildedRose.Tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void TestTheTruth()
+        {
+            Assert.True(true);
+        }
+    }
+}
