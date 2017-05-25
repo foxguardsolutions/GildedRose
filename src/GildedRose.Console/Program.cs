1a591591fe5560da4918d46856b9ce709c561b2f
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-05-25 14:04:57 -0400

    Item Class moved into it's own file
    The Item Class has been moved out of Program.cs into it's own file:
    Item.cs

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -109,16 +109,5 @@ namespace GildedRose.Console
                 }
             }
         }
-
     }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
-
 }
